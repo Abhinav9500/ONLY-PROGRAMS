522141a69c9935612c8704a7c896d75dce625e5a
Abhinav9500 <63339543+Abhinav9500@users.noreply.github.com>
2020-05-02 15:30:56 +0530

    commit

---
+++ fibbonaci.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=0,b=1,c,i,n=150;
+for(i=1;i<=n;i++)
+{
+c=a+b;
+a=b;
+b=c;
+cout<<c;
+}
+return 0;
+
+}
---
+++ simpleprogram.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+    for(int i;i<=100;i++)
+    {cout<<"hello to all"<<endl;
+    
+    }
+    return 0;
+    
+    
+
+}
